525be08879894bbdd90f1a85860c6501f372e82d
PonizhenDoRyadovogo <art25ivan@gmail.com>
2026-02-19 15:51:45 +0700

    Начал работу над кешем с вытеснением, выбран алгоритм LRU (Least Recently Used)

---
+++ LRUCache/LRUCache.cpp
@@ -0,0 +1,33 @@
+﻿#include <unordered_map>
+#include <list>
+#include <mutex>
+#include <string>
+
+template<
+	typename Key, 
+	typename Value, 
+	typename Hash = std::hash<Key>, 
+	typename KeyEqual = std::equal_to<Key>
+>
+class LRUCache {
+public:
+	LRUCache(int capacity) {
+		if (capacity > 10000) {
+			m_capacity = 10000;
+		} esle if (capacity <= 0) {
+			m_capacity = 1;
+		}
+		else {
+			m_capacity = capacity;
+		}
+	}
+private:
+	int m_capacity;
+	mutable std::mutex m_mutex;
+	std::list<std::pair<Key, Value>> m_cacheList;
+	std::unordered_map<
+		Key, typename std::list<std::pair<Key, Value>>::iterator,
+		Hash,
+		KeyEqual
+	> m_cacheMap;
+};
